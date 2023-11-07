4c3e51ae153f2ce769694c9de7ff739bac496f3d
MrZmann <reed.z@utexas.edu>
2023-11-07 09:31:46 -0600

    functions

---
+++ functions.py
@@ -0,0 +1,34 @@
+import random
+import time
+
+# Returns true if the number is even
+# Otherwise returns false
+def is_even(num):
+    pass
+
+# Add test cases
+
+# Returns true if the word contains a vowel
+# Otherwise returns false
+def contains_vowel(word):
+    pass
+
+# Add test cases
+
+def get_average(list_of_numbers):
+    pass
+
+# Add test cases
+
+def get_random_element(list_of_things):
+    pass
+
+# Add test cases
+
+# If t is less than 10, sleep for t seconds
+# Otherwise print "Gotta get back to work soon!" and sleep for 5 seconds
+def snooze(t):
+    pass
+
+# Add test cases
+
